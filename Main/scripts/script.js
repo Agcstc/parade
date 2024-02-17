0309968e30eeae0be2b2b2b571968b7062d422ce
lapcdes <lapcdes@gmail.com>
2024-02-17 12:04:54 -0300

    Parade v0.2

---
+++ Main/scripts/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $('.painting').each(function() {
+            var top_of_element = $(this).offset().top;
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() -500;
+            var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight() -500;
+            var top_of_screen = $(window).scrollTop();
+
+            if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+                // the element is visible, fade it in
+                $(this).css('opacity', '1');
+            } else {
+                // the element is not visible, fade it out
+                $(this).css('opacity', '0');
+            }
+        });
+    });
+});
